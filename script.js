3914f84ed5f3270d055c2c6527fad75bbbd623cc
Yozline <kr1sone@mail.ru>
2022-09-17 21:50:14 +0300

    развелкалово3

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const getNumber = (max = 11, min = 0) => Math.floor(Math.random() * (max - min) 
 const optimizeNumber = (n) =>  (n + "").length === 2 ? n : "0" + n;
 
 const data = [];
-let n = getNumber(41, 1);
+const colors = ["#2EFF9D","#FF00CC","#FF195B","#FFFF69","#2EF2DF"];
+let n = Math.floor(getNumber(41, 1) /6) *6;
 while(n--) {
     data.push(getNumber(100));
 }
@@ -12,5 +13,5 @@ console.log(data);
 const box = document.querySelector(".box");
 
 for (let val of data) {
-    box.innerHTML += `<div class="card">${optimizeNumber(val)}</div>`;
+    box.innerHTML += `<div class="card" style="background-color: ${colors[getNumber(colors.length-1)]}">${optimizeNumber(val)}</div>`;
 }
