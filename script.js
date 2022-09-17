81371f83ab01de94fc272c9234686d1a2a812d15
Yozline <kr1sone@mail.ru>
2022-09-17 21:54:00 +0300

    развелкалово4

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const optimizeNumber = (n) =>  (n + "").length === 2 ? n : "0" + n;
 
 const data = [];
 const colors = ["#2EFF9D","#FF00CC","#FF195B","#FFFF69","#2EF2DF"];
-let n = Math.floor(getNumber(41, 1) /6) *6;
+let n = Math.floor(getNumber(51, 6) /6) *6;
 while(n--) {
     data.push(getNumber(100));
 }
@@ -13,5 +13,5 @@ console.log(data);
 const box = document.querySelector(".box");
 
 for (let val of data) {
-    box.innerHTML += `<div class="card" style="background-color: ${colors[getNumber(colors.length-1)]}">${optimizeNumber(val)}</div>`;
+    box.innerHTML += `<div class="card" style="background-color: ${colors[getNumber(colors.length)-1]}">${optimizeNumber(val)}</div>`;
 }
