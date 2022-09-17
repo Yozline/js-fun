8bc8a1a0988ca91d3f5b9071ce396c71e2a0c910
Yozline <kr1sone@mail.ru>
2022-09-17 22:09:24 +0300

    развелкалово5

--- script.js
+++ script.js
@@ -11,7 +11,28 @@ while(n--) {
 console.log(data);
 
 const box = document.querySelector(".box");
+box.innerHTML = "<div class=\"card\"></div>"
 
 for (let val of data) {
     box.innerHTML += `<div class="card" style="background-color: ${colors[getNumber(colors.length)-1]}">${optimizeNumber(val)}</div>`;
 }
+
+function showTime(day = new Date(2023,0,1)) {
+
+    let now = new Date();
+    let d,h,m,s;
+    let time = Math.floor((day - now) / 1000);
+    m = Math.floor(time / 60);
+    s = time - m * 60;
+    h = Math.floor (m/60);
+    m -= h *60;
+    d = Math.floor(h / 24);
+    h -= d*24;
+    return `${d} д. ${h} ч. ${m} мин. ${s} сек.`
+};
+box.firstElementChild.innerText = `До нового года осталось ${showTime()}`;
+setInterval(function() {
+    box.firstElementChild.innerText = `До нового года осталось ${showTime()}`;
+}, 1000);
+
+console.log(showTime());
